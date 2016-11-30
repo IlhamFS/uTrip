c90583b54ad9cb4afecc607928df00abdf500cd9
Ilham Fathy Saputra <ilhamfathys@gmail.com>
2016-11-30 13:17:32 +0700

    uTrip Landing Page

--- uTrip.py
+++ uTrip.py
@@ -1,9 +1,10 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-   return 'Hello this is uTrip'
+   return render_template('index.html')
 
 if __name__ == '__main__':
    app.run()
