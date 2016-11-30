617c93f7389c9f6b1aeb4973ec976887c0a87a84
Ilham Fathy Saputra <ilhamfathys@gmail.com>
2016-11-30 14:34:23 +0700

    add json

--- uTrip.py
+++ uTrip.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask import render_template
+import json
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-   return render_template('index.html')
+    with open('static/json/places.json') as data_file:
+        data = json.load(data_file)
+        print data;
+    return render_template('index.html', data = data)
 
 if __name__ == '__main__':
    app.run()
