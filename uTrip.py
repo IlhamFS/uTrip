3559f8606bc401ae5021c838223767e167c2ecb1
Ilham Fathy Saputra <ilhamfathys@gmail.com>
2016-12-02 00:46:04 +0700

    fix get query function

--- uTrip.py
+++ uTrip.py
@@ -11,6 +11,7 @@ def main():
         data = json.load(data_file)
     return render_template('index.html', data=data)
 
+
 def get_query_result(location, category, start_hour, end_hour):
     with open('static/json/places.json') as data_file:
         data = json.load(data_file)
@@ -18,16 +19,19 @@ def get_query_result(location, category, start_hour, end_hour):
         places = data["places"]
         result_places = []
         for categories in data["categories"]:
-            if category in categories:
-                id_places = category["id_places"]
+            if categories["name"] == category:
+                id_places = categories["id_places"]
+                break
         for id_place in id_places:
-            open_time = places[id_place]["time"]["open"]
-            close_time = places[id_place]["time"]["close"]
-            locations = places[id_place]["location"]
-            if locations["island"] == location or locations["province"] == location or locations["city"] == location or locations["address"] == location:
-                if not (start_hour > close_time or end_hour < open_time):
-                    result_places.append(places[id_place])
+            place = places[id_place]
+            open_time = place["time"][0]["open"]
+            close_time = place["time"][0]["close"]
+            locations = place["location"]
+            if locations[0]["island"] == location or locations[0]["province"] == location or locations[0]["city"] == location or locations[0]["address"] == location:
+                if not (start_hour >= close_time or end_hour <= open_time):
+                    result_places.append(place)
         return result_places
+
 
 
 if __name__ == '__main__':
