9316208e0ab5e6dd008ee4269ee48605080e5b59
Ilham Fathy Saputra <ilhamfathys@gmail.com>
2016-12-02 00:20:50 +0700

    complete get result function

--- query.py
+++ query.py
@@ -1,11 +1,27 @@
 import json
-from pprint import pprint
 
-with open('Jakarta.json') as data_file:
-    data = json.load(data_file)
 
-startHour = 10
-endHour = 18
-location = "jakarta"
-category = ["Shopping", "Museum"]
-pprint(data)
+def get_query_result(location, category, start_hour, end_hour):
+    with open('static/json/places.json') as data_file:
+        data = json.load(data_file)
+        id_places = []
+        places = data["places"]
+        result_places = []
+        for categories in data["categories"]:
+            if category in categories:
+                id_places = category["id_places"]
+        for id_place in id_places:
+            open_time = places[id_place]["time"]["open"]
+            close_time = places[id_place]["time"]["close"]
+            locations = places[id_place]["location"]
+            if locations["island"] == location or locations["province"] == location or locations["city"] == location or \
+                            locations["address"] == location:
+                if not (start_hour > close_time or end_hour < open_time):
+                    result_places.append(places[id_place])
+        return result_places
+
+
+result = get_query_result("Manado", "Bridges", 10, 12)
+print result
+
+
--- uTrip.py
+++ uTrip.py
@@ -11,18 +11,23 @@ def main():
         data = json.load(data_file)
     return render_template('index.html', data=data)
 
-def get_query_result(location, category, start_hour, end_hour)
+def get_query_result(location, category, start_hour, end_hour):
     with open('static/json/places.json') as data_file:
         data = json.load(data_file)
         id_places = []
         places = data["places"]
         result_places = []
-        for category in data["categories"]:
-            if category["name"] == location:
-                id_places = category["id_palces"]
+        for categories in data["categories"]:
+            if category in categories:
+                id_places = category["id_places"]
         for id_place in id_places:
             open_time = places[id_place]["time"]["open"]
             close_time = places[id_place]["time"]["close"]
+            locations = places[id_place]["location"]
+            if locations["island"] == location or locations["province"] == location or locations["city"] == location or locations["address"] == location:
+                if not (start_hour > close_time or end_hour < open_time):
+                    result_places.append(places[id_place])
+        return result_places
 
 
 if __name__ == '__main__':
