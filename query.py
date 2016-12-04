20674393a063a00ed8127d41891351d0297f2076
Ilham F Saputra <ilhamfathys@gmail.com>
2016-12-04 19:05:34 +0700

    add multiple category in query

--- query.py
+++ query.py
@@ -1,26 +1,24 @@
 import json
 
 
-def get_query_result(location, category, start_hour, end_hour):
-    with open('static/json/places.json') as data_file:
-        data = json.load(data_file)
-        id_places = []
-        places = data["places"]
+def get_query_result(location, categories, start_hour, end_hour):
+    results = {};
+    for category in categories:
         result_places = []
-        for categories in data["categories"]:
-            if categories["name"] == category:
-                id_places = categories["id_places"]
-                break
+        query_category = categories_dict[category]
+        if location in provinces_dict:
+            query_places = provinces_dict[location]
+        else:
+            query_places = cities_dict[location]
+        id_places = list(set(query_places) & set(query_category))
         for id_place in id_places:
             place = places[id_place]
             open_time = place["time"][0]["open"]
             close_time = place["time"][0]["close"]
-            locations = place["location"]
-            if locations[0]["island"] == location or locations[0]["province"] == location or locations[0]["city"] == location or locations[0]["address"] == location:
-                if not (start_hour >= close_time or end_hour <= open_time):
-                    result_places.append(place)
-        return result_places
-
+            if not (start_hour >= close_time or end_hour <= open_time):
+                result_places.append(id_place)
+        results[category] = result_places
+    return results;
 
 result = get_query_result("Manado", "Bridges", 10, 12)
 for r in result:
