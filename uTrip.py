4f255440c039f3a3860e9ca28431cb3ccbe79e80
Ilham Fathy Saputra <ilhamfathys@gmail.com>
2016-12-08 18:00:05 +0700

    change secret key

--- uTrip.py
+++ uTrip.py
@@ -10,6 +10,8 @@ init_build()
 # Application
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
 
 @app.route('/')
 def main():
@@ -72,7 +74,6 @@ def submit_itinerary():
 
 
 if __name__ == '__main__':
-    app.secret_key = 'super secret key'
     app.config['SESSION_TYPE'] = 'filesystem'
 
     app.run(host='0.0.0.0')
