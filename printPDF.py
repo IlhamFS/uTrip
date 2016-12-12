aafc34dd78ba29ebe0db4f53d55c84b47f73a850
febriyola <febriyola.anastasia@gmail.com>
2016-12-12 17:55:45 +0700

    update download

--- printPDF.py
+++ printPDF.py
@@ -2,13 +2,10 @@ import pdfkit
 import re
 import json
 import os
-import wkhtmltopdf
-
-
 
 # febriyola
 def print_pdf(data):
-  appendHtml = "<!DOCTYPE html><html lang=\"en\" xmlns=\"http://www.w3.org/1999/xhtml\"><head><meta charset=\"utf-8\" /><title>uTrip</title>  <link rel=\"stylesheet\" type=\"text/css\" href=\"{{ url_for('static', filename='css/bootstrap.css') }}\"/><link rel=\"stylesheet\" type=\"text/css\" href=\"{{ url_for('static', filename='css/stylesheet.css') }}\" /><link href=\"https://fonts.googleapis.com/css?family=Lobster\" rel=\"stylesheet\"><link href=\"https://fonts.googleapis.com/css?family=Ubuntu\" rel=\"stylesheet\"><script type=\"text/javascript\" src=\"{{ url_for('static', filename='js/jquery-1.12.3.js') }}\"></script><script type=\"text/javascript\" src=\"{{ url_for('static', filename='js/bootstrap.min.js') }}\"></script><script type=\"text/javascript\" src=\"{{ url_for('static', filename='js/typed.js') }}\"></script><script type=\"text/javascript\" src=\"{{ url_for('static', filename='js/my-js.js') }}\"></script></head><body><div><div><div><div align = \"center\"><img  id=\"logo-in-modal\" alt=\"Brand\" src=\"http://ilhamfathy.me/static/img/utrip.png\"><br><h4 id=\"myModalLabel\">Your Itinerary</h4></div><div class=\"modal-body\"><table id=\"itinerary-result\" class=\"result-table table table-bordered table-striped table-hover\"><tr class=\"itinerary-template\"><td class=\"itinerary-time col-md-2\"><b>Time</b></td><td class=\"itinerary-location col-md-3\"><b>Location</b></td><td class=\"itinerary-address col-md-5\"><b>Address</b></td></td><td class=\"itinerary-address col-md-1\"><b>Google Maps</b></td></tr>"
+  appendHtml = "<!DOCTYPE html><html lang=\"en\" xmlns=\"http://www.w3.org/1999/xhtml\"><head><meta charset=\"utf-8\" /><title>uTrip</title>  <link rel=\"stylesheet\" type=\"text/css\" href=\"{{ url_for('static', filename='css/bootstrap.css') }}\"/><link rel=\"stylesheet\" type=\"text/css\" href=\"{{ url_for('static', filename='css/stylesheet.css') }}\" /><link href=\"https://fonts.googleapis.com/css?family=Lobster\" rel=\"stylesheet\"><link href=\"https://fonts.googleapis.com/css?family=Ubuntu\" rel=\"stylesheet\"><script type=\"text/javascript\" src=\"{{ url_for('static', filename='js/jquery-1.12.3.js') }}\"></script><script type=\"text/javascript\" src=\"{{ url_for('static', filename='js/bootstrap.min.js') }}\"></script><script type=\"text/javascript\" src=\"{{ url_for('static', filename='js/typed.js') }}\"></script><script type=\"text/javascript\" src=\"{{ url_for('static', filename='js/my-js.js') }}\"></script></head><body><div><div><div><br><br><br><br><br><div align = \"center\"><img  id=\"logo-in-modal\" alt=\"Brand\" src=\"http://ilhamfathy.me/static/img/utrip.png\"><br><h4 id=\"myModalLabel\">Your Itinerary</h4></div><div class=\"modal-body\"><table id=\"itinerary-result\" class=\"result-table table table-bordered table-striped table-hover\"><tr class=\"itinerary-template\"><td class=\"itinerary-time col-md-2\"><b>Time</b></td><td class=\"itinerary-location col-md-3\"><b>Location</b></td><td class=\"itinerary-address col-md-5\"><b>Address</b></td></td><td class=\"itinerary-address col-md-1\"><b>Google Maps</b></td></tr>"
   for i in data:
       appendHtml += "<tr class=\"itinerary-template\">"
       appendHtml += "<td class=\"itinerary-location col-md-5\"><center>"
@@ -35,17 +32,10 @@ def print_pdf(data):
   appendHtml +="</table><div align = \"center\"><h5>Enjoy your trip!</h5></div></div></div></div></div></body></html>"
 
   options = {
-    'page-size': 'Letter',
-    'margin-top': '0.75in',
-    'margin-right': '0.7in',
-    'margin-bottom': '0.75in',
-    'margin-left': '0.7in',
-    'encoding': "UTF-8",
-    'no-outline': None,
     'zoom':'15',
     'orientation': 'Landscape',
-    'quiet':'false'
   }
+  print "waiting for printing"
 
   css = ['static/css/bootstrap.css', 'static/css/stylesheet.css']  
   pdfkit.from_string(appendHtml, 'static/pdf/itinerary.pdf',options=options, css=css)
@@ -59,4 +49,4 @@ def print_pdf(data):
 
  # path = os.path.abspath('static/pdf/itenerary')
   #print path
-  return jsonify(result={"status": 200})
+  print "done printing"
