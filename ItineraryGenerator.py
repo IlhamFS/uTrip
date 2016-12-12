59445c45074307fed6d992edd3944003162859b0
febriyola <febriyola.anastasia@gmail.com>
2016-12-12 10:20:57 +0700

    gmaps on modals

--- ItineraryGenerator.py
+++ ItineraryGenerator.py
@@ -132,10 +132,16 @@ def string_to_json(string, char_loc, data_json, time_slot):
   result = []
   for idx,i in enumerate(list(string)):
     if not (i=='-'):
-      new_hash = {"type": "normal", "name": char_loc[i], "address": data_json[char_loc[i]]['address'], "time": time_slot[idx] }
+      addr = data_json[char_loc[i]]['address']
+      gmaps = ""
+      if len(addr) != 0:
+        if "near"  not in addr: 
+          gmaps = "<a href=\"https://www.google.com/maps/dir/here/"+addr+"\">Here</a>";
+
+      new_hash = {"type": "normal", "name": char_loc[i], "address": addr, "time": time_slot[idx], "direction": gmaps }
       result.append(new_hash)
     else:
-      new_hash = {"type": "normal", "name": "", "address": "", "time": time_slot[idx] }
+      new_hash = {"type": "normal", "name": "", "address": "", "time": time_slot[idx], "direction": "" }
       result.append(new_hash)
   return result
 
@@ -177,7 +183,7 @@ def get_nearby_resto(data_json, place, eat_time):
 
   elif not (restos_no_add == []):
     rand_resto = random.choice(restos_no_add)
-    return {'name': rand_resto, 'address': ('near '+ place)}
+    return {'name': rand_resto, 'address': ('near '+ place), 'direction': ''}
 
   else:
     return []
@@ -191,6 +197,7 @@ def resto_recommendation(data_json, itin, time, idx, eat_time, itin_place_name):
       itin[idx]['type'] = 'recommendation'
       itin[idx]['name'] = i['name']
       itin[idx]['address'] = i['address'] 
+      itin[idx]['direction'] = ''
   return itin
 
 
